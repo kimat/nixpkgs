03ae73f2724370e97d891213642f3fabe90264df
Kimat Boven <kimat@users.noreply.github.com>
2024-05-15 10:27:30 +0200

    make it possible to override the espanso package

--- nixos/modules/services/desktops/espanso.nix
+++ nixos/modules/services/desktops/espanso.nix
@@ -6,19 +6,27 @@ in {
   meta = { maintainers = with lib.maintainers; [ numkem ]; };
 
   options = {
-    services.espanso = { enable = options.mkEnableOption (lib.mdDoc "Espanso"); };
+    services.espanso = {
+      enable = options.mkEnableOption (lib.mdDoc "Espanso");
+      package = mkOption {
+        type = types.package;
+        default = pkgs.espanso;
+        defaultText = literalExpression "pkgs.espanso";
+        description = mdDoc "The espanso package to use.";
+      };
+    };
   };
 
   config = mkIf cfg.enable {
     systemd.user.services.espanso = {
       description = "Espanso daemon";
       serviceConfig = {
-        ExecStart = "${pkgs.espanso}/bin/espanso daemon";
+        ExecStart = "${cfg.package}/bin/espanso daemon";
         Restart = "on-failure";
       };
       wantedBy = [ "default.target" ];
     };
 
-    environment.systemPackages = [ pkgs.espanso ];
+    environment.systemPackages = [ cfg.package ];
   };
 }
